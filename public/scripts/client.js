5b3397895ba372022afb323b1105a5434fa5f988
KayliedeBlois <kayliedeblois@gmail.com>
2022-07-20 22:15:28 -0400

    Fetching tweets with  Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,56 +38,17 @@ renderTweets = function(data) {
   }
 };
 
-// // Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+// load tweets will create tweet containers for each
+loadTweets = function() {
+  $.get( "/tweets", function( data ) {
+    renderTweets(data);
+  });
+};
 
-// const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-renderTweets(data);
+loadTweets();
 
 // Event Listener for Submit
 $(".insert-tweet").submit(function(event) {
-
   event.preventDefault();
   console.log( $( this ).serialize() );
 
@@ -95,8 +56,7 @@ $(".insert-tweet").submit(function(event) {
     url:'/tweets',
     type:'Post',
     data:$(this).serialize(),
-   
+  
+  });
 });
 
-
-});
